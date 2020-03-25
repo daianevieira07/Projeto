b5b2ac188c7e9462a57b63177515f5e804019337
Daiane Vieira <61718653+daianevieira07@users.noreply.github.com>
2020-03-24 21:50:38 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,99 +1,118 @@
-let name = prompt("Qual é o seu nome?", "name" );
 
-//let name = document.getElementById ("name")
+let name = prompt("Qual é o seu nome?", "name");
 
-//name.innerHtml = "Olá " + name + "!";
+let nomeHTML = document.getElementById("nome-pessoa")
 
-document.write("Olá " + name +"!\n")
+nomeHTML.innerHTML = "Olá " + name + "!";
 
 
-let jogar = prompt( "Oi " + name + "! Deseja jogar? \n Digite o número: \n 1- Sim \n 2- Não")
+let jogar = prompt("Oi " + name + "! Deseja jogar? \n Digite o número: \n 1- Sim \n 2- Não")
 
 if (jogar != "1") {
-    mensagemSair = "A operação foi cancelada. Agradecemos a sua visita!"
-    alert(mensagemSair)
-    document.write(mensagemSair) 
-    document.innerHtml
+    let sairHTML = document.getElementById("mensagemSair")
+    sairHTML.innerHTML = "A operação foi cancelada. Agradecemos a sua visita!";
+    alert("A operação foi cancelada. Agradecemos a sua visita!")
 }
 
 else {
     let pergunta1 = prompt("Qual é a capital da França? \n 1- Nice \n 2- Paris \n 3-Budapeste  ")
+    if (pergunta1 > "3" || pergunta1 === "") {
+        alert("Só digita: 1, 2 ou 3 pra responder.")
+        let pergunta1 = prompt("Qual é a capital da França? \n 1- Nice \n 2- Paris \n 3-Budapeste  ")
+    }
+
     let pergunta2 = prompt("Qual é a capital do Brasil? \n 1- Salvador \n 2- Brasilia \n 3-São Paulo")
+    if (pergunta2 > "3" || pergunta2 === "") {
+        alert("Só digita: 1, 2 ou 3 pra responder.")
+        let pergunta2 = prompt("Qual é a capital do Brasil? \n 1- Salvador \n 2- Brasilia \n 3-São Paulo")
+    }
+
     let pergunta3 = prompt("Qual é a capital dos Estados Unidos? \n 1- Nova York \n 2- Washington \n 3-Maiami")
+    if (pergunta3 > "3" || pergunta3 === "") {
+        alert("Só digita: 1, 2 ou 3 pra responder.")
+        let pergunta3 = prompt("Qual é a capital dos Estados Unidos? \n 1- Nova York \n 2- Washington \n 3-Maiami")
+
+    }
 
     switch (pergunta1) {
 
         case "1":
-
-            document.write  (" Resposta 1 errada <br>")
+            let resposta1case1 = document.getElementById("respostaE1case1")
+            resposta1case1.innerHTML = "Resposta 1 - Errada";
             break;
 
         case "2":
-            document.write(" Resposta 1 certa <br>")
+            let resposta1case2 = document.getElementById("respostaC1")
+            resposta1case2.innerHTML = "Resposta 1 - Correta";
             break;
 
         case "3":
+            let resposta3case3 = document.getElementById("respostaE1case3")
+            resposta3case3.innerHTML = "Resposta 1 - Errada";
 
-            document.write(" Resposta 1 errada <br> ")
+                            
+      default:
+            confirm("Está tentando sair do jogo?")
 
-            break;
-
-        default:
-            alert("Digite um número para a opção: 1, 2, ou 3.");
-            break;
-    }
-
-
-
-    switch (pergunta2) {
-
-        case "1":
-            document.write("Resposta 2 errada <br>")
-            break;
-
-        case "2":
-            document.write("Resposta 2 certa <br>")
-
-            break;
-
-        case "3":
-            document.write("Resposta 2 errada <br>")
-
-            break;
-
-        default:
-            alert("Digite um número para a opção: 1, 2, ou 3.");
-            break;
-    }
+            if (confirm = true) {
+                {
+                    let sairHTML = document.getElementById("mensagemSair")
+                    sairHTML.innerHTML = "A operação foi cancelada. Agradecemos a sua visita!";
+                    alert("A operação foi cancelada. Agradecemos a sua visita!")
+                }
+                break;
+            }
 
 
-    switch (pergunta3) {
 
-        case "1":
-            document.write("Resposta 3 errada <br>")
-            break;
+            switch (pergunta2) {
 
-        case "2":
-            document.write("Resposta 3 certa <br>")
-            break;
+                case "1" , "3":
+                    let resposta2case1 = document.getElementById("respostaE2case1")
+                    resposta2case1.innerHTML = "Resposta 2 - Errada";
 
-        case "3":
-            document.write("Resposta 3 errada <br>")
+                    break;
 
-            break;
+                case "2":
+                    let resposta2case2 = document.getElementById("respostaC2")
+                    resposta2case2.innerHTML = "Resposta 2 - Correta";
 
-        default:
-            alert("Digite um número para a opção: 1, 2, ou 3.");
-            break;
+                    break;
+
+               case "3":
+                    let resposta2case3 = document.getElementById("respostaE2case3")
+                    resposta2case3.innerHTML = "Resposta 2 - Errada";
+
+                    break;
+
+                default:
+                    
+                    break;
+            }
+
+
+            switch (pergunta3) {
+
+                case "1" , "3":
+                    let resposta3case1 = document.getElementById("respostaE3case1")
+                    resposta3case1 .innerHTML = "Resposta 3 - Errada";
+                    break;
+
+                case "2":
+                    let resposta3case2 = document.getElementById("respostaC3")
+                    resposta3case2.innerHTML = "Resposta 3 - Correta";
+                    break;
+
+                case "3":
+                    let resposta3case3 = document.getElementById("respostaE3case3")
+                    resposta3case3.innerHTML = "Resposta 3 - Errada";
+
+                    break;
+
+                default:
+                
+                    break;
+            }
     }
 }
-
-
-document.getElementsById("corretas")
-
-
-document.getElementsById("erradas")
-
-
-
 
